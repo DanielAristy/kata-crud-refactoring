5371b88fc19ba08a175cb10f3e3254d174614420
DanielAristy <danielaristy22@gmail.com>
2021-06-02 14:30:20 -0500

    Refactorizando la clase Todo con solo los atributos en este momento de id, name, completed

--- back/src/main/java/co/com/sofka/crud/model/Todo.java
+++ back/src/main/java/co/com/sofka/crud/model/Todo.java
@@ -1,8 +1,6 @@
 package co.com.sofka.crud.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Todo {
@@ -11,37 +9,23 @@ public class Todo {
     private Long id;
     private String name;
     private boolean completed;
-    private String groupListId;
-
-    public String getGroupListId() {
-        return groupListId;
-    }
-
-    public void setGroupListId(String groupListId) {
-        this.groupListId = groupListId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public boolean isCompleted() {
         return completed;
     }
-
     public void setCompleted(boolean completed) {
         this.completed = completed;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Long getId() {
+        return id;
     }
 }
